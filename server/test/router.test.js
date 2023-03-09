67739cf956d4fe3ffab9c2d2ebcb615f8cc21438
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-09 16:28:26 +0100

    feat: folder for tests

---
+++ server/test/router.test.js
@@ -0,0 +1,13 @@
+import { describe, expect, test } from '@jest/globals';
+import { router } from '../router';
+import { app } from '../index';
+
+const BASE_URL = 'http://localhost:5000'
+
+
+
+describe('Router module - Tagline', () => {
+  it('GET /getTagline responds posted taglines, status 200', done => {
+
+  })
+})
